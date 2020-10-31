50ab9a270d15af6285f3dc417b12cc1ccdde83de
Farhad Hossain <farhad17hossain@gmail.com>
2020-10-31 22:24:06 +0600

    module 4 solution

---
+++ harder/SpeakHello.js
@@ -0,0 +1,10 @@
+(function(window) {
+	var helloSpeaker ={speak:function(name) {
+		console.log(speakWord + " " + name);
+	}
+};
+window.helloSpeaker=helloSpeaker;
+var speakWord = "Hello";
+
+
+})(window);
